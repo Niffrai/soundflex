3bff166ceca3683007dbed7100f64d6dab9fa04a
Niffrai <mrniffrai3115@gmail.com>
2023-12-20 20:17:25 +0300

    first commit

---
+++ SoundEf1/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Media;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace SoundEf1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            SoundPlayer sp = new SoundPlayer("C:\\Users\\as31l\\source\\repos\\SoundEf1\\Sound\\soundforest.wav");
+            sp.Play();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_7(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
